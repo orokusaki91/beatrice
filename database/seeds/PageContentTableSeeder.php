88a2cccdb2c950dcebd170fd845808d9ae849cd9
orokusaki91 <31542903+orokusaki91@users.noreply.github.com>
2018-05-10 09:41:36 +0200

    add seeders for beatrice

--- database/seeds/PageContentTableSeeder.php
+++ database/seeds/PageContentTableSeeder.php
@@ -14,60 +14,69 @@ class PageContentTableSeeder extends Seeder
     {
         $pageContent = new PageContent;
         $pageContent->page_id = 1;
-        $pageContent->image = '5.jpg';
-        $pageContent->text = '<h1><strong>HERZLICH WILLKOMMEN</strong></h1><h2><strong><span style="color: #000000;">AUF UNSERER WEBSEITE.&nbsp;</span><br /></strong></h2><h3><strong>WIR SIND IHR ANPRECHPARTNER&nbsp;<br />F&Uuml;R LOGISTIK IN DER SCHWEIZ.</strong></h3>';
+        $pageContent->image = 'UNVv6ATohlt4MnojjpUo72qwW9GNUdodboGVVc9D.jpeg';
+        $pageContent->text = '<p style="color: #000000;"><span style="color: #ffffff;">Resilienztraining</span></p>
+<p style="color: #000000;"><span style="color: #ffffff;">Beratung in der Praxis</span></p>
+<p style="color: #000000;"><span style="color: #ffffff;">Theaterp&auml;dagogik</span></p>
+<p style="color: #000000;"><span style="color: #ffffff;">Mediation</span></p>
+<p style="color: #000000;"><span style="color: #ffffff;">Dipl.Pflegefachfrau HF</span></p>
+<p style="color: #000000;"><span style="color: #ffffff;">und einiges mehr...</span></p>';
         $pageContent->page_code_id = 1;
         $pageContent->save();
 
         $pageContent = new PageContent;
         $pageContent->page_id = 2;
-        $pageContent->image = '1.jpg';
-        $pageContent->text = '<p>Wir besch&auml;ftigen im Moment 16 Fahrzeuge von Lieferwagen bis H&auml;nger Zug und Sattelschlepper f&uuml;r die St&uuml;ckgutverteilung in der Schweiz.</p>
-<p>Bedient wird t&auml;glich die ganze Schweiz ausser das Wallis, den Tessin und Graub&uuml;nden. Diese 3 Regionen beliefern wir mit unseren regionalen Partnern innert 48 Stunden.</p>
-<ul>
-<li>Unser Umschlagslager hat eine Grundfl&auml;che von rund 2000 m2</li>
-<li>Zus&auml;tzlich Stellpl&auml;tze in den Regalen 1100 Paletten</li>
-<li>B&uuml;hne f&uuml;r Lager oder kommisionier Arbeiten 350 m2</li>
-<li>Rampenpl&auml;tze f&uuml;r Kleinfahrzeuge 4</li>
-<li>Rampenpl&auml;tze f&uuml;r LKW\'s 15</li>
-</ul>
-<p>Der komplette Lagerraum ist vom Zollamt Pratteln f&uuml;r externe Spediteure als zugelassener Empf&auml;nger abgenommen und bewilligt.</p>
-<p>Gerne erarbeiten wir Ihnen, eine auf Ihre Bed&uuml;rfnisse ausgearbeitete Offerte f&uuml;r unsere Dienstleistungen zur Verf&uuml;gung.</p>';
+        $pageContent->image = 'sVBrUYSOqpHHmonck4P76Eae4sYJU3xP1BwoEfgq.png';
+        $pageContent->text = '<p style="color: #000000;"><span style="color: #ffffff;">Mich faszinieren Menschen und ihre vielf&auml;ltigsten Motive.</span></p>
+<p style="color: #000000;"><span style="color: #ffffff;">Humor, Spiel, Ernst, nachdenken....</span></p>
+<p style="color: #000000;"><span style="color: #ffffff;">Ver&auml;nderung geschieht immer wieder.</span></p>
+<p style="color: #000000;"><span style="color: #ffffff;">Ich kann Sie dabei, durch verschiedenste Methoden, unterst&uuml;tzen.</span></p>';
         $pageContent->page_code_id = 5;
         $pageContent->save();
 
         $pageContent = new PageContent;
         $pageContent->page_id = 3;
-        $pageContent->image = '4.jpg';
-        $pageContent->text = '<h1>Kein Weg ist zu schwer</h1>';
+        $pageContent->image = 'BEH4bgiUo5Z86jyNsCtwDlDSal0UCDhWeStVdHvC.jpeg';
+        $pageContent->text = '<h1 style="color: #000000;">"All our dreams can come true,&nbsp;<br />if we have the courage to pursue them."</h1>
+<div class="nastavak_main" style="color: #000000;">
+<p>Walt Disney</p>
+</div>';
         $pageContent->page_code_id = 6;
         $pageContent->save();
 
         $pageContent = new PageContent;
         $pageContent->page_id = 4;
-        $pageContent->image = 'road_sign_rectangal_blank.png';
-        $pageContent->title = 'Transport Schweiz';
-        $pageContent->text = '<p>Wir beliefern die ganze Schweiz mit St&uuml;ckguttransport oder als Teil- und Ganzwagenladung. Auch bieten wir Extrafahrten f&uuml;r St&uuml;ckguttransporte. Geniessen Sie von unserer hohen Dienstleistungsqualit&auml;t.</p>
-<ul>
-<li>Bereitstellung von qualitativ hochwertigen Transportdienstleistungen f&uuml;r alle unsere Kunden</li>
-<li>Wir investieren in unsere Angestellten, um einen besseren Service und Unternehmenswachstum zu bieten</li>
-<li>Wir sorgen uns um unsere Umwelt mit den neuesten Standards unserer Branche</li>
-<li>Die Sicherheit hat bei uns oberste Priorit&auml;t bei der Gew&auml;hrleistung sicherer Arbeitsabl&auml;ufe</li>
-<li>Wir investieren in neueste Technologien, um schnelle, pr&auml;zise und kosteneffektive Dienstleistungen zu bieten</li>
-</ul>';
+        $pageContent->image = '7minWfiN9Pv08c1jaPxs2R9HN739yhB3NceI6GUn.jpeg';
+        $pageContent->title = 'Lorem ipsum';
+        $pageContent->text = '<p style="color: #000000;">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi, cupiditate facere ducimus sit sequi consequuntur ut deserunt cumque. Accusantium, quod.</p>
+<p style="color: #000000;">Mich faszinieren Menschen und ihre vielf&auml;ltigsten Motive.</p>';
         $pageContent->page_code_id = 7;
         $pageContent->save();
 
         $pageContent = new PageContent;
         $pageContent->page_id = 4;
-        $pageContent->image = 'road_sign_rectangal_blank.png';
-        $pageContent->title = 'Lagerung'; 
-        $pageContent->text = '<ul>
-<li>Wir verf&uuml;gen &uuml;ber rund 2000m2 Grundfl&auml;che f&uuml;r den Umschlag und Lagerung von G&uuml;tern.</li>
-<li>Zus&auml;tzliche Stellpl&auml;tze f&uuml;r 1100 Paletten.</li>
-<li>Wir vermieten Lagerfl&auml;chen individuell nach Kundenwunsch.</li>
-<li>Wir verwalten das Lager zuverl&auml;ssig und kompetent.</li>
-</ul>';
+        $pageContent->image = '01ucd3Govz6QBKyzrwc9H5ICyu21j3SNYReMsnTE.jpeg';
+        $pageContent->title = 'Lorem ipsum'; 
+        $pageContent->text = '<p style="color: #000000;">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi, cupiditate facere ducimus sit sequi consequuntur ut deserunt cumque. Accusantium, quod.</p>
+<p style="color: #000000;">Humor, Spiel, Ernst, nachdenken....</p>';
+        $pageContent->page_code_id = 7;
+        $pageContent->save();
+
+        $pageContent = new PageContent;
+        $pageContent->page_id = 4;
+        $pageContent->image = 'yLVxoj2UczR9vgYvVc8QCJHimGRxshGTEaWWJVrf.jpeg';
+        $pageContent->title = 'Lorem ipsum'; 
+        $pageContent->text = '<p style="color: #000000;">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi, cupiditate facere ducimus sit sequi consequuntur ut deserunt cumque. Accusantium, quod.</p>
+<p style="color: #000000;">Ver&auml;nderung geschieht immer wieder.</p>';
+        $pageContent->page_code_id = 7;
+        $pageContent->save();
+
+        $pageContent = new PageContent;
+        $pageContent->page_id = 4;
+        $pageContent->image = 'dmDtxfLPPAzhcIj2ZCLxz2g5O1gL6oBZ4z0eKzUW.png';
+        $pageContent->title = 'Lorem ipsum'; 
+        $pageContent->text = '<p style="color: #000000;">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi, cupiditate facere ducimus sit sequi consequuntur ut deserunt cumque. Accusantium, quod.</p>
+<p style="color: #000000;">Ich kann Sie dabei, durch verschiedenste Methoden, unterst&uuml;tzen.</p>';
         $pageContent->page_code_id = 7;
         $pageContent->save();
     }
