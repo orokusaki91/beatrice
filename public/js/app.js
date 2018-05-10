0a8142b801ecbee3af520e31557561804e5d4379
orokusaki91 <31542903+orokusaki91@users.noreply.github.com>
2018-05-10 09:58:27 +0200

    add dropdown js for admin

--- public/js/app.js
+++ public/js/app.js
@@ -66,6 +66,20 @@ $(function() {
     });
 });
 
+var dropdown = document.getElementsByClassName("dropdown-btn");
+var i;
+for (i = 0; i < dropdown.length; i++) {
+    dropdown[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var dropdownContent = this.nextElementSibling;
+        if (dropdownContent.style.display === "block") {
+            dropdownContent.style.display = "none";
+        } else {
+            dropdownContent.style.display = "block";
+        }
+    });
+}
+
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("header-left-sredina-open").style.marginLeft= "0";
