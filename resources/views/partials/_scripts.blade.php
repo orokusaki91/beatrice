307b8127c6a57114d16fcf4157235bac35a9bbe4
orokusaki91 <31542903+orokusaki91@users.noreply.github.com>
2018-06-12 20:20:29 +0200

    fix scripts

--- resources/views/partials/_scripts.blade.php
+++ resources/views/partials/_scripts.blade.php
@@ -1,3 +1,3 @@
-<script src="http://code.jquery.com/jquery-2.1.3.min.js"></script>
+<script src="https://code.jquery.com/jquery-2.1.3.min.js"></script>
 <script src="{{ asset('js/owl.carousel.min.js') }}"></script>
 <script src="{{ asset('js/app.js?ver=' . str_random(10)) }}"></script>
