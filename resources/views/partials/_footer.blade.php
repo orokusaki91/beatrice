b634f9c114ca4d0e53e8cd33cf7166ac769e6b85
dotlicweb <38036937+dotlicweb@users.noreply.github.com>
2018-06-18 12:47:03 +0200

    css / footer

--- resources/views/partials/_footer.blade.php
+++ resources/views/partials/_footer.blade.php
@@ -1,9 +1,7 @@
 <div id="footer">
 	<div class="footer">
-  		<a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
-        <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
-        <a href="#"><i class="fa fa-youtube" aria-hidden="true"></i></a>
-        <a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+        <a href="https://www.facebook.com/Beratung-und-mehrch-314701422032382/" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+        <a href="https://www.linkedin.com/m/login/" target="_blank"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
   	</div>
 </div>
 <div class="copi">
